abbb9a6a0522b29d476ac6e208f944c067f89994
NDT28092005 <ndaitung28092005@gmail.com>
2025-12-17 08:54:17 +0700

    abc

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173','https://proud-mud-098aeae00.1.azurestaticapps.net'],
+    'allowed_origins' => ['http://localhost:5173',
+     'http://localhost:3000', 
+     'https://proud-mud-098aeae00.1.azurestaticapps.net',
+    ],
 
     'allowed_origins_patterns' => [],
 
